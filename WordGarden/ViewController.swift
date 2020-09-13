b167b998eb4e7d0a1d876d568c6a45dd1866484e
Jackie Cochran <cochrajg@bc.edu>
2020-09-13 15:18:49 -0400

    Week 3 Word Garden App

--- WordGarden/ViewController.swift
+++ WordGarden/ViewController.swift
@@ -22,15 +22,42 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let text = guessedLetterField.text!
+        guessLetterButton.isEnabled = !(text.isEmpty)
+        
     }
 
+    func updateUIAfterGuess(){
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+        guessLetterButton.isEnabled = false
+    }
 
     @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        //This dismisses the keyboard
+       updateUIAfterGuess()
     }
     
     @IBAction func playAgainButtonPressed(_ sender: UIButton) {
     }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        updateUIAfterGuess()
+    }
+    
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        var text = guessedLetterField.text!
+        if let lastCharacter = text.last{
+            text = String(lastCharacter)
+        }else{
+           text = ""
+        }
+        guessLetterButton.isEnabled = !(text.isEmpty)
+
+        
+        
+    }
+    
     
 }
 
